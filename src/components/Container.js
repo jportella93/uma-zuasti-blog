3335b40d61bcd443b32a66dfce721b9d50c9e101
jportella93 <jportella93@gmail.com>
2019-09-11 15:38:19 +0200

    style: switch pure css-in-js for styled components

---
+++ src/components/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  padding-left: 10px;
+  padding-right: 10px;
+
+  @media (min-width: 768px) {
+    max-width: 1140px;
+  }
+`
+
+export default Container
