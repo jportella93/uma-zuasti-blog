2de8372c6baab7b4fbdfa4df7808900925db8206
jportella93 <jportella93@gmail.com>
2019-09-11 14:02:56 +0200

    feat: add some css-in-js global styles

---
+++ src/components/styles.js
@@ -0,0 +1,10 @@
+export const smallContainerStyle = {
+  paddingLeft: "10px",
+  paddingRight: "10px",
+}
+
+export const bigContainerStyle = {
+  paddingLeft: "10px",
+  paddingRight: "10px",
+  maxWidth: "1140px"
+}
