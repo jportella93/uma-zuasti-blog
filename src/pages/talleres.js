74a8aaa4cef7b4a605aa63627e6d05b3ac1f991a
jportella93 <jportella93@gmail.com>
2019-09-11 14:02:23 +0200

    chore: clean up index and add placeholder pages

--- src/pages/talleres.js
+++ src/pages/talleres.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-export default class IndexPage extends React.Component {
+export default class TalleresPage extends React.Component {
   render() {
 
     return (
       <Layout>
         <section className="section">
           <div className="container">
-            Hello index
+            Hello talleres
           </div>
         </section>
       </Layout>
