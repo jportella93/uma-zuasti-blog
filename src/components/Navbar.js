7dc5458ce31ecea96b8bac85aba8634f09dbfdc1
jportella93 <jportella93@gmail.com>
2019-09-11 16:35:04 +0200

    feat: update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,109 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 import logo from '../img/logo.svg'
+import {Basic, Limited} from '../components/Containers'
+import styled from 'styled-components'
+import {smallScreenLimit} from '../components/constants'
+import useResizeAware from 'react-resize-aware';
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand" style={{display: 'flex', flexDirection: 'column'}}>
-        <Link to="/" className="navbar-item"
-        style={{margin: '0 auto'}}>
-          <figure className="image">
-            <img src={logo} alt="Uma Zuasti, profesora de biodanza, número de registro: PV 1621."
-            style = {{maxHeight: 'unset'}}/>
-          </figure>
-        </Link>
-          <p style={{fontSize: '9px', margin: '0 auto'}}>PROFESORA DE BIODANZA NÚMERO DE REGISTRO: PV 1621.</p>
-      </div>
-    </div>
-  </nav>
-)
+const NavFoldedHeight = '90px';
 
+const NavContainer = styled(Basic)`
+  position: fixed;
+  width: 100%;
+  background: gray;
+`
+
+const NavPusher = styled.div`
+  height: ${NavFoldedHeight};
+`
+
+const Nav = styled(Limited)`
+  margin: auto;
+  max-height: ${props => props.isMenuOpen ? "200px" : NavFoldedHeight};
+  transition: max-height 1s ease;
+  overflow: hidden;
+`
+
+const TopLevelDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const FoldableSubmenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const SubmenuContainer = styled.div`
+  display: flex;
+  width: 55%;
+  justify-content: space-between;
+`
+
+const MenuItemBigScreen = styled(Link)`
+  color: #fafafa;
+`
+
+const MenuItemSmallScreen = styled(Link)`
+  color: #567123;
+`
+
+const submenuItemIds = [
+  'Servicios',
+  'Publicaciones',
+  'Talleres',
+  'Testimonios',
+  'Contacto',
+]
+
+const getSubmenuItems = (isSmallScreen) => {
+  const MenuItem = isSmallScreen ? MenuItemSmallScreen : MenuItemBigScreen;
+  return submenuItemIds.map(id =>
+    <MenuItem key={id} to={`/${id.toLowerCase()}`}>{id}</MenuItem>)
+}
+
+const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = React.useState(false);
+  const [resizeListener, sizes] = useResizeAware();
+
+  const showSmallScreenMenu = sizes.width < smallScreenLimit;
+
+  return (
+    <>
+      <NavContainer>
+        <Nav isMenuOpen={isMenuOpen}>
+          {resizeListener}
+          <TopLevelDiv>
+            <Link to="/">
+              <img src={logo} alt="Uma Zuasti" />
+            </Link>
+            {!showSmallScreenMenu &&
+              <SubmenuContainer>
+                {getSubmenuItems(false)}
+              </SubmenuContainer>
+            }
+            {showSmallScreenMenu &&
+              <button
+                className={`hamburger hamburger--spring ${isMenuOpen ? 'is-active' : ''}`}
+                onClick={() => setMenuOpen(!isMenuOpen)}
+                type="button">
+                <span className="hamburger-box">
+                  <span className="hamburger-inner"></span>
+                </span>
+              </button>
+            }
+          </TopLevelDiv>
+          {showSmallScreenMenu &&
+            <FoldableSubmenuContainer>
+              {getSubmenuItems(true)}
+            </FoldableSubmenuContainer>
+          }
+        </Nav>
+      </NavContainer>
+      <NavPusher />
+    </>
+  )
+}
 export default Navbar
---
+++ src/components/constants.js
@@ -0,0 +1,1 @@
+export const smallScreenLimit = 768;
