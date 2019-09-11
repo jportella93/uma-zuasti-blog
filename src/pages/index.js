84d966bb91a6c6ec62f69c7638415116028b3abc
jportella93 <jportella93@gmail.com>
2019-09-11 18:48:38 +0200

    feat: index prototype

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,51 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styled from 'styled-components'
+import jumpImg from '../img/jump.png'
 
-export default class IndexPage extends React.Component {
-  render() {
+const Page = styled.div`
+  position: relative;
+`
 
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            Hello index
-          </div>
+const TextContainer = styled.div`
+  position: absolute;
+  top: 30%;
+  left: 0;
+  right: 0;
+  width: 60%;
+  text-align: center;
+  margin: auto;
+`
+
+const Title = styled.h1`
+  color: #fafafa;
+  font-weight: bold;
+  font-size: 1.5rem;
+`
+
+const Quote = styled.h2`
+  color: #fafafa;
+`
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Page>
+        <section>
+          <img src={jumpImg} alt="Gente saltando." />
+          <TextContainer>
+            <Title>PSICOPEDAGOGÍA CORPORAL</Title>
+            <br />
+            <Quote>
+              La Psique de una persona y su musculatura voluntaria son funcionalmente equivalentes
+            <br />
+              Whilhem Reich
+            </Quote>
+          </TextContainer>
         </section>
-      </Layout>
-    )
-  }
+      </Page>
+    </Layout>
+  )
 }
+
+export default IndexPage
