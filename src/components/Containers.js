32291f57f6619ed2677eecaaf27e6df5f7b91e97
jportella93 <jportella93@gmail.com>
2019-09-11 15:42:24 +0200

    feat: add Basic and Limited containers

--- src/components/Containers.js
+++ src/components/Containers.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
-const Container = styled.div`
+export const Basic = styled.div`
   padding-left: 10px;
   padding-right: 10px;
+`
 
+export const Limited = styled(Basic)`
   @media (min-width: 768px) {
     max-width: 1140px;
   }
 `
-
-export default Container
