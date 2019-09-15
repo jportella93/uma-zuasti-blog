c10861ed663dff214e025d7a0efa23f43267a826
jportella93 <jportella93@gmail.com>
2019-09-15 18:07:21 +0200

    feat: add default heading TextStyles

---
+++ src/components/TextStyles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const defaultH1 = styled.h1`
+font-size: 2rem;
+`
+
+export const defaultH2 = styled.h2`
+font-size: 1.5rem;
+`
+
+export const defaultH3 = styled.h3`
+font-size: 1.17rem;
+`
+
+export const defaultH4 = styled.h4`
+font-size: 1.12rem;
+`
+
+export const defaultH5 = styled.h5`
+font-size: .83rem;
+`
+
+export const defaultH6 = styled.h6`
+font-size: .75rem;
+`
