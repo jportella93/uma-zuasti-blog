bb143cb2c73f5d80e520fe58571583a932525e0a
jportella93 <jportella93@gmail.com>
2019-09-15 18:18:33 +0200

    feat: add margins and correct naming

--- src/components/TextStyles.js
+++ src/components/TextStyles.js
@@ -1,25 +1,35 @@
 import styled from 'styled-components';
 
-export const defaultH1 = styled.h1`
+export const H1 = styled.h1`
 font-size: 2rem;
+margin: .67rem 0;
 `
 
-export const defaultH2 = styled.h2`
+export const H2 = styled.h2`
 font-size: 1.5rem;
+margin: .75rem 0;
 `
 
-export const defaultH3 = styled.h3`
+export const H3 = styled.h3`
 font-size: 1.17rem;
+margin: .83rem 0;
 `
 
-export const defaultH4 = styled.h4`
+export const H4 = styled.h4`
 font-size: 1.12rem;
+margin: 1.12rem 0;
 `
 
-export const defaultH5 = styled.h5`
+export const H5 = styled.h5`
 font-size: .83rem;
+margin: 1.5rem 0 ;
 `
 
-export const defaultH6 = styled.h6`
+export const H6 = styled.h6`
 font-size: .75rem;
+margin: 1.67rem 0;
+`
+
+export const P = styled.p`
+margin: 1.12rem 0;
 `
