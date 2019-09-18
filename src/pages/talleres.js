7c2e1f3407c8c4d0d0c7612a917cdb3a96ad866d
jportella93 <jportella93@gmail.com>
2019-09-19 00:07:03 +0200

    feat: TalleresView prototype

--- src/pages/talleres.js
+++ src/pages/talleres.js
@@ -1,17 +1,74 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import {graphql} from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Helmet from 'react-helmet';
+import styled from 'styled-components';
+import BlogPostCard from '../components/BlogPostCard';
+import {BlogLimitedContainer} from '../components/Containers';
+import Layout from '../components/Layout';
+import {H1} from '../components/TextStyles';
 
-export default class TalleresPage extends React.Component {
-  render() {
+const Title = styled(H1)`
+  margin-bottom: 24px;
+  margin-top: 24px;
+`
 
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            Hello talleres
-          </div>
-        </section>
-      </Layout>
-    )
-  }
+const TalleresView = ({data}) => {
+
+  const {edges: posts} = data.allMarkdownRemark
+
+  return (
+    <Layout>
+      <Helmet title={`Talleres | ${data.site.siteMetadata.title}`} />
+      <BlogLimitedContainer>
+        <Title>Próximos talleres</Title>
+        {
+          posts
+          .map(({node: post}) => (
+            <BlogPostCard key={post.id} post={post} />
+          ))
+        }
+      </BlogLimitedContainer>
+    </Layout>
+  )
 }
+
+TalleresView.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export default TalleresView;
+
+export const pageQuery = graphql`
+  query TalleresQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] },
+      filter: { frontmatter: { templateKey: { eq: "blog-post" }, tags: { in: "Talleres"} }}
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            tags
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
