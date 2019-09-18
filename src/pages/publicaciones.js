0682d1f1c3b32368e273ad2eef2ae981c2653cac
jportella93 <jportella93@gmail.com>
2019-09-19 00:22:33 +0200

    fix: filter PublicacionesView query

--- src/pages/publicaciones.js
+++ src/pages/publicaciones.js
@@ -47,7 +47,7 @@ export const pageQuery = graphql`
   query PublicacionesQuery {
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      filter: { frontmatter: { templateKey: { eq: "blog-post" }, tags: {in: "Artículos"} }}
     ) {
       edges {
         node {
