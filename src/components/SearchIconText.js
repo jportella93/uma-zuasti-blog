50d51963d7f20b2524be916f40eb3005a78094e2
jportella93 <jportella93@gmail.com>
2019-11-26 09:00:11 +0100

    feat: add SearchIcontext

---
+++ src/components/SearchIconText.js
@@ -0,0 +1,19 @@
+import { Link } from "gatsby";
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "../img/search-white.svg";
+
+const Separator = styled.div`
+  padding-bottom: 24px;
+`
+
+const SearchIconText = ({ link, children }) => (
+  <Link to={link}>
+    <img src={SearchIcon} />
+    <br />
+    {children}
+    <Separator />
+  </Link>
+);
+
+export default SearchIconText;
