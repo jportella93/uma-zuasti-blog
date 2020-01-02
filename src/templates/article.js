b5d5d636892160ae62af25a803cb24b80c589aa1
jportella93 <jportella93@gmail.com>
2020-01-02 22:05:15 +0100

    feat: article template

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Helmet from 'react-helmet';
+import styled from 'styled-components';
+import { LimitedContainer } from '../components/Containers';
+import Content, { HTMLContent } from '../components/Content';
+import Layout from '../components/Layout';
+import { palette } from '../components/constants';
 
 export const ArticleTemplate = ({
   content,
@@ -17,31 +19,13 @@ export const ArticleTemplate = ({
   const PostContent = contentComponent || Content
 
   return (
-    <section className="section">
+    <section className="content">
       {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
+      <h1>
+        {title}
+      </h1>
+      <p>{description}</p>
+      <PostContent content={content} />
     </section>
   )
 }
@@ -54,19 +38,32 @@ ArticleTemplate.propTypes = {
   helmet: PropTypes.instanceOf(Helmet),
 }
 
+const ArticleContainer = styled(LimitedContainer)`
+  padding-top: 40px;
+  padding-bottom: 40px;
+  border-top: 10px solid ${palette.red};
+  border-bottom: 10px solid ${palette.red};
+  text-align: unset;
+`
+
 const Article = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
-    <Layout>
-      <ArticleTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
-        tags={post.frontmatter.tags}
-        title={post.frontmatter.title}
-      />
+    <Layout
+      bgColor={palette.white}
+      navbarColor={palette.red}
+    >
+      <ArticleContainer>
+        <ArticleTemplate
+          content={post.html}
+          contentComponent={HTMLContent}
+          description={post.frontmatter.description}
+          helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
+          tags={post.frontmatter.tags}
+          title={post.frontmatter.title}
+        />
+      </ArticleContainer>
     </Layout>
   )
 }
