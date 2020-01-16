c5c7c59f19fd2e6bbfee0b354ce1095116884197
jportella93 <jportella93@gmail.com>
2020-01-16 22:09:47 +0100

    feat: add missing service pages

---
+++ src/pages/servicios/parto-y-movimiento.js
@@ -0,0 +1,33 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import ServiceView from '../../components/ServiceView';
+
+const PartoYMovimientoView = ({ data }) => {
+  data.title = "Parto y Movimiento"
+
+  return <ServiceView {...data} />
+}
+
+export const pageQuery = graphql`
+  query {
+    workshopsQ: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: {templateKey: {eq: "workshop"}, productType: {eq: "parto-y-movimiento"}}}) {
+      edges {
+        node{
+          ...Article
+        }
+      }
+    }
+    articlesQ: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: {templateKey: {eq: "article"}, productType: {eq: "parto-y-movimiento"}}}) {
+      edges {
+        node{
+          ...Article
+        }
+      }
+    }
+    featuredImageQ: file(name: {eq: "4-parto-y-movimiento-1"}) {
+      ...Fluid1200
+    }
+  }
+`
+
+export default PartoYMovimientoView;
