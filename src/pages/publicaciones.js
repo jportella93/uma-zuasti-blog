018fcc24ee3aa1b3b8bb7e8d6b1932b4c46bdb0d
jportella93 <jportella93@gmail.com>
2020-01-19 21:49:13 +0100

    feat: publicaciones page

--- src/pages/publicaciones.js
+++ src/pages/publicaciones.js
@@ -1,74 +1,27 @@
-import {graphql} from 'gatsby';
-import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 import React from 'react';
-import Helmet from 'react-helmet';
-import styled from 'styled-components';
-import BlogPostCard from '../components/BlogPostCard';
-import {BlogLimitedContainer} from '../components/Containers';
-import Layout from '../components/Layout';
-import {H1} from '../components/TextStyles';
+import PublicationListView from '../components/PublicationListView';
 
-const Title = styled(H1)`
-  margin-bottom: 24px;
-  margin-top: 24px;
-`
-
-const PublicacionesView = ({data}) => {
-
-  const {edges: posts} = data.allMarkdownRemark
-
-  return (
-    <Layout>
-      <Helmet title={`Publicaciones | ${data.site.siteMetadata.title}`} />
-      <BlogLimitedContainer>
-        <Title>Publicaciones más recientes</Title>
-        {
-          posts
-          .map(({node: post}) => (
-            <BlogPostCard key={post.id} post={post} />
-          ))
-        }
-      </BlogLimitedContainer>
-    </Layout>
+const PublicacionesView = ({
+  data: { publicationsQ: { edges: publications }, }
+}) => (
+    <PublicationListView
+      publications={publications}
+      title="Publicaciones"
+      readMoreText="Leer más ->"
+    />
   )
-}
-
-PublicacionesView.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
-
-export default PublicacionesView;
 
 export const pageQuery = graphql`
-  query PublicacionesQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" }, tags: {in: "Artículos"} }}
-    ) {
+  query {
+    publicationsQ: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: { templateKey: {eq: "article"}}}) {
       edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            tags
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
+        node{
+          ...Article
         }
       }
     }
-  }
+}
 `
+
+export default PublicacionesView;
