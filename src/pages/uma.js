0a78833a1b5cb72619f1d62925f0c117be6f774a
Jon Portella <jportella93@gmail.com>
2021-10-23 10:34:10 -0400

    update copy

--- src/pages/uma.js
+++ src/pages/uma.js
@@ -9,7 +9,8 @@ import { H1, P } from "../components/TextStyles";
 const textBlocks = [
   `Terapeuta, educadora infantil e investigadora.`,
   `Postgrado en Psicología Perinatal, pionera en Biodanza Perinatal.`,
-  `Profesora de Biodanza, nº de Registro: PV 1621.`,
+  `Profesora-Didacta de Biodanza, nº de Registro: PV 1621.`,
+  `Tutora en la Escuela de Formación de Profesionales de Biodanza SRT de Castellón AYUN.`,
   `Especialista en las Aplicaciones "Biodanza para niños, adolescentes y familia" formada por Nadia Costa y  "Biodanza para gestantes" por Claudette Sant´Anna, Scuolatoro UNIPIB Milán (Italia). También en las extensiones "Biodanza y arcilla” y  "El camino del Heroe", trabajo corporal sobre algunos de los arquetipos post-junguianos.`,
   `Arte-teraputa de orientación Junguiana.`,
   `Terapeuta en PHMA, (Phyco Motor Healing Activitis) y SEL (Somatic emocional liberation), herramientas post trauma utilizadas con niños en los campos de refugiados de Turquía y Lebanon.`,
@@ -41,7 +42,7 @@ const UmaPage = ({ data }) => {
   const imageTextBlocks = [
     {
       fluidSrc: fluidSrcs[0],
-      contentSlot: getContentsForTextBlocks(0, 3),
+      contentSlot: getContentsForTextBlocks(0, 4),
       imgProps: {
         style: {
           maxWidth: '300px',
@@ -52,11 +53,11 @@ const UmaPage = ({ data }) => {
     },
     {
       fluidSrc: fluidSrcs[1],
-      contentSlot: getContentsForTextBlocks(3, 6)
+      contentSlot: getContentsForTextBlocks(4, 7)
     },
     {
       fluidSrc: fluidSrcs[2],
-      contentSlot: getContentsForTextBlocks(6)
+      contentSlot: getContentsForTextBlocks(7)
     },
   ]
 
