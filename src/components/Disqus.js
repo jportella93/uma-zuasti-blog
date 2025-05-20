f6dcc8fcdb4e242262454243ca78ff379d145a60
Jon Portella <jportella93@gmail.com>
2025-05-20 17:21:50 +0200

    Add Spanish language support in gatsby-ssr and Disqus component

--- src/components/Disqus.js
+++ src/components/Disqus.js
@@ -2,6 +2,6 @@ import { DiscussionEmbed } from "disqus-react";
 import React from "react";
 
 const Disqus = ({ identifier, title }) =>
-  <DiscussionEmbed shortname={'umazuasti'} config={{ identifier, title }} />
+  <DiscussionEmbed shortname={'umazuasti'} config={{ identifier, title, language: 'es' }} />
 
 export default Disqus;
